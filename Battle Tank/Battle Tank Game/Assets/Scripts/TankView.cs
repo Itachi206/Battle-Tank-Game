b8747b79da76943c21094a83c29674871fbc8e4f
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-04-23 20:48:10 +0530

    added camera to follow tank

--- Battle Tank/Battle Tank Game/Assets/Scripts/TankView.cs
+++ Battle Tank/Battle Tank Game/Assets/Scripts/TankView.cs
@@ -16,7 +16,12 @@ public class TankView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //getting the gameobject of name Main Camera using find
+        GameObject camera = GameObject.Find("Main Camera");
+        //setting the tranform of the gameobject of this script to the parent of the camera
+        camera.transform.SetParent(transform);
+        //setting the trasfrom position value of the camera
+        camera.transform.position = new Vector3(0f, 4f, -5f);
     }
 
     // Update is called once per frame
